9a4d51fb7c4f3723e0c37953cfef1cd3a4aa374a
Anderson Bruno <andersonblsantos@Outlook.com>
2023-07-27 21:39:22 -0300

    Added removed item action

--- src/store/reducers/bag.ts
+++ src/store/reducers/bag.ts
@@ -4,11 +4,16 @@ import { mockStores } from '../../mock/stores';
 interface IItems {
     quantity: number,
     itemId: number,
-    comment: string
+    comment: string,
+    price: number,
+    name: string,
+    description: string,
+    id: number
 }
 
 interface IBag {
     storeId: number,
+    storeName: string,
     TotalPrice: number,
     TotalItems: number,
     items: IItems[]
@@ -21,8 +26,15 @@ interface ItemPayload {
     comment: string;
 }
 
+interface ItemRemovedPayload {
+    quantity: number,
+    id: number,
+    price: number
+}
+
 const initialState: IBag = {
     storeId: 0,
+    storeName: '',
     TotalPrice: 0,
     TotalItems: 0,
     items: []
@@ -32,7 +44,11 @@ const bagSlice = createSlice({
     name: 'bag',
     initialState,
     reducers: {
-        clearBag: () => initialState,
+        clearBag: () => {
+            localStorage.setItem('bag', JSON.stringify(initialState));
+
+            return initialState;
+        },
         setBag: (state, action: PayloadAction<IBag>) => {
             return state = action.payload;
         },
@@ -45,12 +61,32 @@ const bagSlice = createSlice({
 
             const newTotalPrice = state.storeId == storeId ? state.TotalPrice + (item.price * quantity) : (item.price * quantity);
             const newTotalItems = state.storeId == storeId ? state.TotalItems + (quantity) : quantity;
-            const newItems = state.storeId == storeId ? [...state.items, {itemId: item.id, quantity, comment}] : [{itemId: item.id, quantity, comment}];
+            const newItems = state.storeId == storeId ? [...state.items, {itemId: item.id, quantity, comment, price: item.price, name: item.name, description: item.description, id: Math.random()}] : [{itemId: item.id, quantity, comment, price: item.price, name: item.name, description: item.description, id: Math.random()}];
 
             const newState = { 
                 storeId: store?.id || 0,
+                storeName: store.name,
                 TotalItems: newTotalItems,
                 TotalPrice: newTotalPrice,
+                items: newItems
+            };
+
+            localStorage.setItem('bag', JSON.stringify(newState));
+
+            return state = newState;
+        },
+        removeItem: (state, action: PayloadAction<ItemRemovedPayload>) => {
+            const { id, quantity, price } = action.payload;
+
+            const newItems = state.items.filter((item) => {
+                return item.id !== id
+            });
+
+            const newState = { 
+                storeId: state.storeId,
+                storeName: state.storeName,
+                TotalItems: state.TotalItems - quantity,
+                TotalPrice: state.TotalPrice - (price * quantity),
                 items: newItems
             };
 
@@ -61,6 +97,6 @@ const bagSlice = createSlice({
     }
 });
 
-export const { clearBag, addItem, setBag } = bagSlice.actions;
+export const { clearBag, addItem, setBag, removeItem } = bagSlice.actions;
 
 export default bagSlice.reducer;
